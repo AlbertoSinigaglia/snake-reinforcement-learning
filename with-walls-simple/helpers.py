028abf4929bc019afd4adf5b778b8cd74920cd45
Alberto Sinigaglia <sinigagliaalberto@gmail.com>
2023-01-01 03:32:58 +0100

    attempt to use graph mode, no luck, will delete in next commit

--- with-walls-simple/helpers.py
+++ with-walls-simple/helpers.py
@@ -61,3 +61,39 @@ def re_normalize_possible_actions(boards, probs, mask_with=0.):
     if np.size(idx_left): mask[idx_left, BaseEnvironment.LEFT] = mask_with
 
     return tf.linalg.normalize(probs * mask, ord=1, axis=-1)[0]
+
+
+def get_mask(boards, probs, mask_with=0.):
+    heads = np.argwhere(boards == BaseEnvironment.HEAD)
+    walls = np.argwhere(boards == BaseEnvironment.WALL)
+    mask = np.ones_like(probs)
+
+    heads_up = np.copy(heads)
+    heads_down = np.copy(heads)
+    heads_left = np.copy(heads)
+    heads_right = np.copy(heads)
+
+    heads_up[:, 1] += 1
+    heads_down[:, 1] -= 1
+    heads_right[:, 2] += 1
+    heads_left[:, 2] -= 1
+
+    powers = np.max(walls)**np.arange(walls.shape[1])
+
+    walls_code = np.sum(walls*powers, axis=-1)
+    heads_up_code = np.sum(heads_up*powers, axis=-1)
+    heads_down_code = np.sum(heads_down*powers, axis=-1)
+    heads_left_code = np.sum(heads_left*powers, axis=-1)
+    heads_right_code = np.sum(heads_right*powers, axis=-1)
+
+    idx_up = heads_up[np.isin(heads_up_code, walls_code)][:,0]
+    idx_down = heads_up[np.isin(heads_down_code, walls_code)][:,0]
+    idx_right = heads_up[np.isin(heads_right_code, walls_code)][:,0]
+    idx_left = heads_up[np.isin(heads_left_code, walls_code)][:,0]
+
+    if np.size(idx_up): mask[idx_up, BaseEnvironment.UP] = mask_with
+    if np.size(idx_down): mask[idx_down, BaseEnvironment.DOWN] = mask_with
+    if np.size(idx_right): mask[idx_right, BaseEnvironment.RIGHT] = mask_with
+    if np.size(idx_left): mask[idx_left, BaseEnvironment.LEFT] = mask_with
+
+    return mask
